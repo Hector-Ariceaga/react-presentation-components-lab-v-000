9e1462b9b85ee69e74e63003c1d44d6eda0e343a
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 21:57:05 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -3,6 +3,9 @@ import React from 'react'
 
 const SimplerComponent = props => {
 
+  handleClick = () => {
+    <div>wow</div>
+  }
   return(
     <div onClick={props.handleClick}>I am just happy</div>
   )
