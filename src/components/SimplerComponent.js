b8d9412c50b8bf1d265fe7db6d729785d38ce1dd
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 21:48:57 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,7 @@
 // Code SimplerComponent Here
 import React, {Component} from 'react';
-export default class SimpleComponent extends Component {
+
+export default class SimplerComponent extends Component {
 
   handleClick = () => {
     <div>So nice I can say it twice!</div>
