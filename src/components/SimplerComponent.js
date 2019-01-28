e46b82256ff706a2bb8f81e1c7b8cf8b73ecce89
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 21:43:57 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,6 +1,9 @@
 // Code SimplerComponent Here
 const SimpleComponent = () => {
+  handleClick = (e) => {
+    e.target.append('!')
+  }
   return(
-    <div>I am just happy</div>
+    <div onClick={this.handleClick}>I am just happy</div>
   )
 }
