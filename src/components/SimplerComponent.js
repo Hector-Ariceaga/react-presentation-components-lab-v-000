c043c9909e74af9c3981bc3b06bd4ebd19da7442
Hector Ariceaga <hector_ariceaga@hotmail.com>
2019-01-28 21:46:56 +0000

    Automatically backed up by Learn

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,12 +1,13 @@
 // Code SimplerComponent Here
 
 const SimpleComponent = () => {
+  
   handleClick = () => {
     return <div>'So nice I can say it twice!'</div>
   }
 
   return(
-    <div onClick={this.handleClick}>'I am just happy'</div>
+    <div onClick={this.handleClick}>I am just happy</div>
   )
 }
 
